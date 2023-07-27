cab475efd94c92f85fa7fb8479f99b8cdd94e82b
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-27 15:03:05 +0200

    feat: add prompts (pari e dispari)

---
+++ Pari e Dispari/main.js
@@ -0,0 +1,7 @@
+/*Chiedo all'utente di scrivere pari o dispari*/
+const userAnswer = prompt("Scegli tra pari o dispari?");
+console.log(userAnswer);
+
+/*Chiedo all'utente di scegliere un numero da 1 a 5 */
+const userNumber = prompt("Scegli un numero da 1 a 5");
+console.log(userNumber);
