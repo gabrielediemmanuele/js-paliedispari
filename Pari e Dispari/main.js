355af8e8a236ef62374e0dd8b757cba940f6408f
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-27 16:59:06 +0200

    fix: pari e dispari comments

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -11,11 +11,6 @@ console.log(userNumber);
 
 let player, computer;
 
-/* Tramite una funzione, creo un numero da 1 a 5 al pc */
-function getRandomNumber() {
-  computerNumber = Math.floor(Math.random() * 5 + 1);
-  return computerNumber;
-}
 const generatedNumber = getRandomNumber();
 console.log(generatedNumber);
 
@@ -23,34 +18,18 @@ console.log(generatedNumber);
 let totalNumber = userNumber + generatedNumber;
 console.log(totalNumber);
 
-let results;
-/*stabilire se la somma dei due numeri è pari o dispari*/
-function pariDispariWinner() {
-  if (totalNumber % 2 == 0) {
-    results = totalNumber;
-    console.log("pari");
-  } else {
-    results = totalNumber;
-    console.log("dispari");
-  }
-  return results;
+const totalNumberWinner = isEven(totalNumber);
+if (totalNumberWinner == true) {
+  console.log("Il numero è pari");
+} else {
+  console.log("Il numero è dispari");
 }
 
-const totalNumberWinner = pariDispariWinner();
-console.log(results);
-
-/* STABILIRE IL VINCITORE.... */
 // ? Se il risultato è pari e l'utente ha scritto 'pari'...
 // ? Altrimenti se il risultato è dispari e l utente ha scritto dispari..
-/* if (results % 2 == 0 && e pari) {
-    alert('hai vinto');
-} else if (results % 2 !== 0 e dispari){
-    alert('hai vinto');
-} else {
-    alert ('hai perso');
-} */
 
-if (results % 2 == 0) {
+/* STABILIRE IL VINCITORE.... */
+if (totalNumber % 2 == 0) {
   if (userAnswer == "pari") {
     console.log("hai vinto");
   } else {
@@ -63,5 +42,20 @@ if (results % 2 == 0) {
     console.log("hai perso");
   }
 }
-// passare la somma e restituire una parola o "pari" o
+
+// passare la "somma" (totalNumber) e restituire una parola o "pari" o
 // "dispari" da confrontare con la parola scelta dall'utente
+/* Tramite una funzione, creo un numero da 1 a 5 al pc */
+function getRandomNumber() {
+  computerNumber = Math.floor(Math.random() * 5 + 1);
+  return computerNumber;
+}
+
+/*funzione che verifica se la somma è pari o dispari*/
+function isEven(value) {
+  let results = false;
+  if (value % 2 == 0) {
+    results = true;
+  }
+  return results;
+}
